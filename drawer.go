48a3430da547fa5bdc020c22e1a86abbabd7421f
Korondr <109851440+Korondr@users.noreply.github.com>
2025-10-01 22:01:41 +0300

    Update drawer.go

--- drawer.go
+++ drawer.go
@@ -1,1 +1,56 @@
+package main
+
+import "fmt"
+
+var hangmanStates = []string{
+	`
+	+---+
+	    |
+	    |
+	    |
+	   ===`,
+	`
+	+---+
+	O   |
+	    |
+	    |
+	   ===`,
+	`
+	+---+
+	O   |
+	|   |
+	    |
+	   ===`,
+	`
+	+---+
+	  O   |
+   /|   |
+	      |
+	   ===`,
+	`
+	+---+
+	  O   |
+   /|\  |
+	      |
+	   ===`,
+	`
+	+---+
+	  O   |
+   /|\  |
+   /    |
+	   ===`,
+	`
+	+---+
+	  O   |
+   /|\  |
+   / \  |
+	   ===`,
+}
+
+func DrawHangman(mistakes int) {
+	if mistakes >= len(hangmanStates) {
+		mistakes = len(hangmanStates) - 1
+	}
+	fmt.Println(hangmanStates[mistakes])
+}
 
