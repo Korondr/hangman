4cd3f01e2365d6a27d0bea20fb07ec9debd8b5a2
Korondr <109851440+Korondr@users.noreply.github.com>
2025-10-01 22:01:02 +0300

    Update dictionary.go

--- dictionary.go
+++ dictionary.go
@@ -1,1 +1,29 @@
+package main
+
+import "math/rand"
+
+var wordCategories = map[string][]string{
+	"animals": {"elephant", "tiger", "cat", "dog"},
+	"fruits":  {"apple", "banana", "orange"},
+}
+
+func Categories() []string {
+	cats := make([]string, 0, len(wordCategories))
+	for k := range wordCategories {
+		cats = append(cats, k)
+	}
+	return cats
+}
+
+func RandWord(category string) string {
+	words, ok := wordCategories[category]
+	if !ok || len(words) == 0 {
+		// случайная категория
+		for _, w := range wordCategories {
+			words = w
+			break
+		}
+	}
+	return words[rand.Intn(len(words))]
+}
 
